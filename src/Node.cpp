224f5bc7c2b8b4ab8663862c94402da40ddfb4b5
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2014-10-10 21:35:17 +0200

    added makefile, Node, test main

---
+++ src/Node.cpp
@@ -0,0 +1,20 @@
+/*
+ * Node.cpp
+ *
+ * Copyright (c) 2014, Alessandro Pezzato
+ */
+
+#include "Node.h"
+
+namespace swiftree {
+
+Node::Node() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Node::~Node() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace swiftree */
---
+++ src/Node.h
@@ -0,0 +1,20 @@
+/*
+ * Node.h
+ *
+ * Copyright (c) 2014, Alessandro Pezzato
+ */
+
+#ifndef swiftree_NODE_H_
+#define swiftree_NODE_H_
+
+namespace swiftree {
+
+class Node {
+public:
+	Node();
+	virtual ~Node();
+};
+
+} /* namespace swiftree */
+
+#endif /* swiftree_NODE_H_ */
